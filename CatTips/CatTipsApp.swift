96794c6f2606cad636973d83e3aafa6fca0d0f43
Cityhunter <johan.guenaoui@gmail.com>
2023-08-02 17:38:53 +0200

    Initial Commit

---
+++ CatTips/CatTipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatTipsApp.swift
+//  CatTips
+//
+//  Created by Johan Guenaoui on 02/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct CatTipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
