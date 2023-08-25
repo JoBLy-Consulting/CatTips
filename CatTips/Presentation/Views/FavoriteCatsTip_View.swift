efa18b257166450a465bc39409248ded0640c3f9
Cityhunter <johan.guenaoui@gmail.com>
2023-08-25 18:42:30 +0200

    Add Favorites Model management

---
+++ CatTips/Presentation/Views/FavoriteCatsTip_View.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteCatsTip_View.swift
+//  CatTips
+//
+//  Created by Johan Guenaoui on 25/08/2023.
+//
+
+import SwiftUI
+
+struct FavoriteCatsTip_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteCatsTip_View()
+}
