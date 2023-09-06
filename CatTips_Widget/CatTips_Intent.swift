808035dc586abf9e0404c4501a0394eca5ff8a32
Cityhunter <johan.guenaoui@gmail.com>
2023-09-06 19:05:34 +0200

    Add an interactive widget

---
+++ CatTips_Widget/CatTips_Intent.swift
@@ -0,0 +1,8 @@
+//
+//  CatTips_Intent.swift
+//  CatTips_WidgetExtension
+//
+//  Created by Johan Guenaoui on 06/09/2023.
+//
+
+import Foundation
---
+++ CatTips_Widget/CatTips_WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  CatTips_WidgetBundle.swift
+//  CatTips_Widget
+//
+//  Created by Johan Guenaoui on 06/09/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CatTips_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CatTips_Widget()
+    }
+}
